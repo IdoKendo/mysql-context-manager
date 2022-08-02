8b0fa16e722c936429b7fe7b4be008c56db2581b
IdoKendo <ryuusuke@gmail.com>
2022-08-02 10:15:07 +0300

    Separate fixture to conftest

--- tests/test_mysql_context_manager.py
+++ tests/test_mysql_context_manager.py
@@ -3,19 +3,6 @@ from mysql_context_manager import __version__
 from mysql_context_manager import MysqlConnector
 
 
-@pytest.fixture()
-def mock_db(mocker):
-    mocker.patch("databases.Database.connect", return_value=None)
-    mocker.patch("databases.Database.disconnect", return_value=None)
-    mocker.patch(
-        "databases.Database.fetch_all",
-        return_value=[
-            [("username", "Aang")],
-        ],
-    )
-    mocker.patch("databases.Database.execute", return_value=None)
-
-
 def test_version():
     assert __version__ == "0.1.2"
 
