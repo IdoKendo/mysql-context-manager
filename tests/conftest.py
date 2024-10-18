b766a109a805d0d1bf92da76f86a20730ac2ab93
IdoKendo <ryuusuke@gmail.com>
2024-10-18 14:00:24 +0300

    chore: align pre-commit hooks and run with --all-files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture
 def _mock_db(mocker):
     mocker.patch("databases.Database.connect", return_value=None)
     mocker.patch("databases.Database.disconnect", return_value=None)
